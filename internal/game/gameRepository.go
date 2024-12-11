296a170cec2a86ec3a5056b9cb18543226c4e9cf
Keiji Lohier <kl6456@g.rit.edu>
2024-12-10 20:53:41 -0500

    Add Game Repository

--- internal/game/gameRepository.go
+++ internal/game/gameRepository.go
@@ -1,1 +1,14 @@
 package game
+
+import (
+	"context"
+)
+
+type GameRepository interface {
+	FindAll(ctx context.Context) ([]Game, error)
+	FindByID(ctx context.Context, id int) (*Game, error)
+	Create(ctx context.Context, player1 int, player2 int, board_size int) (*Game, error)
+	GetGrids(ctx context.Context, gameId int) ([]Box, error)
+	UpdateGrid(ctx context.Context, gameId int, row int, col int, edge string) ([]Box, error)
+	IsEdgeSelected(ctx context.Context, gameId int, row int, col int, edge string) (bool, error)
+}
