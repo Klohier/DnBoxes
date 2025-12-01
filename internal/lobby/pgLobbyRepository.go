05f11146a96a2b84460bd91eac2df02653e3c4b7
Keiji Lohier <keiji.lohier@gmail.com>
2025-12-01 17:25:39 -0500

    comment out old lobby implementation

--- internal/lobby/pgLobbyRepository.go
+++ internal/lobby/pgLobbyRepository.go
@@ -1,52 +1,52 @@
 package lobby
 
-import (
-	"context"
-	"fmt"
+// import (
+// 	"context"
+// 	"fmt"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-)
+// 	"github.com/jackc/pgx/v5/pgxpool"
+// )
 
-type PgLobbyRepository struct {
-	db *pgxpool.Pool
-}
+// type PgLobbyRepository struct {
+// 	db *pgxpool.Pool
+// }
 
-func NewPgLobbyRepository(db *pgxpool.Pool) *PgLobbyRepository {
-	return &PgLobbyRepository{
-		db: db,
-	}
-}
+// func NewPgLobbyRepository(db *pgxpool.Pool) *PgLobbyRepository {
+// 	return &PgLobbyRepository{
+// 		db: db,
+// 	}
+// }
 
-func (repo *PgLobbyRepository) FindAll(ctx context.Context) ([]Lobby, error) {
-	var lobbies []Lobby
+// func (repo *PgLobbyRepository) FindAll(ctx context.Context) ([]Lobby, error) {
+// 	var lobbies []Lobby
 
-	query := `SELECT lobby_id, name, host_user_id, created_at, session_id, is_private FROM lobbies`
-	rows, err := repo.db.Query(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+// 	query := `SELECT lobby_id, name, host_user_id, created_at, session_id, is_private FROM lobbies`
+// 	rows, err := repo.db.Query(ctx, query)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
 
-	for rows.Next() {
-		var lobby Lobby
-		if err := rows.Scan(&lobby.LobbyID, &lobby.Name, &lobby.HostID, &lobby.CreatedAt, &lobby.SessionID, &lobby.IsPrivate); err != nil {
-			return nil, err
-		}
-		lobbies = append(lobbies, lobby)
-		if err := rows.Err(); err != nil {
-			return nil, err
-		}
+// 	for rows.Next() {
+// 		var lobby Lobby
+// 		if err := rows.Scan(&lobby.LobbyID, &lobby.Name, &lobby.HostID, &lobby.CreatedAt, &lobby.SessionID, &lobby.IsPrivate); err != nil {
+// 			return nil, err
+// 		}
+// 		lobbies = append(lobbies, lobby)
+// 		if err := rows.Err(); err != nil {
+// 			return nil, err
+// 		}
 
-	}
-	return lobbies, nil
-}
+// 	}
+// 	return lobbies, nil
+// }
 
-func (repo *PgLobbyRepository) FindByID(ctx context.Context, lobbyId int) (*Lobby, error) {
-	var lobby Lobby
-	query := `SELECT name, host_user_id, created_at, session_id, is_private FROM lobbies where lobby_id = $1`
-	err := repo.db.QueryRow(ctx, query, lobbyId).Scan(&lobby.Name, &lobby.HostID, &lobby.CreatedAt, &lobby.SessionID, &lobby.IsPrivate)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find game %d : %w", lobbyId, err)
-	}
-	return &lobby, nil
-}
+// func (repo *PgLobbyRepository) FindByID(ctx context.Context, lobbyId int) (*Lobby, error) {
+// 	var lobby Lobby
+// 	query := `SELECT name, host_user_id, created_at, session_id, is_private FROM lobbies where lobby_id = $1`
+// 	err := repo.db.QueryRow(ctx, query, lobbyId).Scan(&lobby.Name, &lobby.HostID, &lobby.CreatedAt, &lobby.SessionID, &lobby.IsPrivate)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to find game %d : %w", lobbyId, err)
+// 	}
+// 	return &lobby, nil
+// }
