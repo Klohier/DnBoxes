6f81dd38259f3015637996de2c05d06742645fbf
Keiji Lohier <keiji.lohier@gmail.com>
2025-12-17 12:30:21 -0500

    Update register to match login

--- web/src/routes/register.tsx
+++ web/src/routes/register.tsx
@@ -1,35 +1,46 @@
 import { useAuth } from "@/AuthContext";
 import { AuthForm } from "@/components/AuthForm";
+import { LoginCredentials } from "@/types/auth";
 import {
   createFileRoute,
-  useNavigate,
+  redirect,
   useRouter,
   useRouterState,
 } from "@tanstack/react-router";
+import z from "zod";
+
+const fallback = "/" as const;
 
 export const Route = createFileRoute("/register")({
-  component: RouteComponent,
+  validateSearch: z.object({
+    redirect: z.string().optional().catch(""),
+  }),
+  beforeLoad: ({ context, search }) => {
+    if (context.authentication.isAuthenticated) {
+      redirect({ to: search.redirect ?? fallback, throw: true });
+    }
+  },
+  component: Register,
 });
 
-function RouteComponent() {
+function Register() {
   const { register } = useAuth();
+  const search = Route.useSearch();
   const router = useRouter();
-  const navigate = useNavigate();
   const isLoading = useRouterState({ select: (s) => s.isLoading });
+  const navigate = Route.useNavigate();
 
-  async function handleRegister(values: {
-    username: string;
-    password: string;
-  }) {
+  async function onSubmit(values: LoginCredentials) {
     await register(values);
     await router.invalidate();
-    await navigate({ to: "/" });
+    await navigate({ to: search.redirect ?? fallback });
   }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <AuthForm
-          onSubmitHandler={handleRegister}
+          onSubmitHandler={onSubmit}
           isLoading={isLoading}
           buttonText="Register"
           formType="register"
